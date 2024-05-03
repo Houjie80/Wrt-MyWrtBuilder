789133b39b7a586c0cf868d5fda314504d10587a
Houjie80 <86527356+Houjie80@users.noreply.github.com>
2024-05-04 01:36:52 +0700

    Update mac80211.sh

--- files/lib/wifi/mac80211.sh
+++ files/lib/wifi/mac80211.sh
@@ -209,7 +209,7 @@ detect_mac80211() {
 			set wireless.default_${name}.device=${name}
 			set wireless.default_${name}.network=lan
 			set wireless.default_${name}.mode=ap
-			set wireless.default_${name}.ssid=NOKIA N73
+			set wireless.default_${name}.ssid=NOKIA-N73
 			set wireless.default_${name}.encryption=psk2
                         set wireless.default_${name}.key=sijitekowolu
 EOF
