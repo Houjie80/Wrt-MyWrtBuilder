9a41404057b161a0fb1fc267e6d4ef108a2549ac
Houjie80 <86527356+Houjie80@users.noreply.github.com>
2024-08-24 10:54:36 +0700

    Update tunnel_app.sh

--- scripts/tunnel_app.sh
+++ scripts/tunnel_app.sh
@@ -21,12 +21,12 @@ passwall2_api="https://api.github.com/repos/rtaserver/openwrt-passwall2/releases
 passwall2_file="luci-23.05_luci-app-passwall"
 passwall2_file_down="$(curl -s ${passwall2_api} | grep "browser_download_url" | grep -oE "https.*${passwall2_file}.*.ipk" | head -n 1)"
 
-passwall_ipk_packages=("https://github.com/lrdrdn/my-opkg-repo/raw/main/$ARCH_3/dns2tcp_1.1.0-1_$ARCH_3.ipk"
-                       "https://github.com/lrdrdn/my-opkg-repo/raw/main/$ARCH_3/chinadns-ng_2023.06.01-1_$ARCH_3.ipk"
-                       "https://github.com/lrdrdn/my-opkg-repo/raw/main/$ARCH_3/xray-plugin_1.8.4-1_$ARCH_3.ipk"
-                       "https://github.com/lrdrdn/my-opkg-repo/raw/main/$ARCH_3/trojan-plus_10.0.3-2_$ARCH_3.ipk"
-                       "https://github.com/lrdrdn/my-opkg-repo/raw/main/$ARCH_3/ipt2socks_1.1.3-3_$ARCH_3.ipk"
-                       "https://github.com/lrdrdn/my-opkg-repo/raw/main/$ARCH_3/pdnsd-alt_1.2.9b-par-3_$ARCH_3.ipk")
+#passwall_ipk_packages=("https://github.com/lrdrdn/my-opkg-repo/raw/main/$ARCH_3/dns2tcp_1.1.0-1_$ARCH_3.ipk"
+                     #  "https://github.com/lrdrdn/my-opkg-repo/raw/main/$ARCH_3/chinadns-ng_2023.06.01-1_$ARCH_3.ipk"
+                     #  "https://github.com/lrdrdn/my-opkg-repo/raw/main/$ARCH_3/xray-plugin_1.8.4-1_$ARCH_3.ipk"
+                      # "https://github.com/lrdrdn/my-opkg-repo/raw/main/$ARCH_3/trojan-plus_10.0.3-2_$ARCH_3.ipk"
+                      # "https://github.com/lrdrdn/my-opkg-repo/raw/main/$ARCH_3/ipt2socks_1.1.3-3_$ARCH_3.ipk"
+                      # "https://github.com/lrdrdn/my-opkg-repo/raw/main/$ARCH_3/pdnsd-alt_1.2.9b-par-3_$ARCH_3.ipk")
                        
 
 if [ "$1" == "openclash" ]; then
